8f0e4233b513734640fd09459e590341fe7944ea
EgleVas <98325656+EgleVas@users.noreply.github.com>
2024-05-13 17:04:39 +0300

    Resolve Lint errors

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -2,22 +2,22 @@ require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient
 const fs = require('fs')
 
-let url = `${process.env.MONGO_URL}`
-let filename = `${__dirname}/secondChanceItems.json`
+const url = `${process.env.MONGO_URL}`
+const filename = `${__dirname}/secondChanceItems.json`
 const dbName = 'secondChance'
 const collectionName = 'secondChanceItems'
 const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs
 
-async function loadData() {
+async function loadData () {
   const client = new MongoClient(url)
   try {
     await client.connect()
     console.log('Connected successfully to server')
     const db = client.db(dbName)
     const collection = db.collection(collectionName)
-    let cursor = await collection.find({})
-    let documents = await cursor.toArray()
-    if (documents.length == 0) {
+    const cursor = await collection.find({})
+    const documents = await cursor.toArray()
+    if (documents.length === 0) {
       const insertResult = await collection.insertMany(data)
       console.log('Inserted documents:', insertResult.insertedCount)
     } else {
@@ -33,5 +33,5 @@ async function loadData() {
 loadData()
 
 module.exports = {
-    loadData
+  loadData
 }
